651d801d72edeb7d383b58b72dbdb2769aa45ef4
vladgibeskul <wgv96300@gmail.com>
2024-12-25 08:54:53 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{/// <summary>
+///  Одна штука некоторого товара стоит 20,4 руб. Напечатать таблицу стоимости 2, 3, ..., 20 штук этого товара.
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        public static void Func()
+        {
+            double price = 20.4;
+            for (int i = 2; i <= 20; i++)
+            {
+                Console.WriteLine($"{i}-ий.={price * i}руб");
+            }
+        }
+    }
+}
