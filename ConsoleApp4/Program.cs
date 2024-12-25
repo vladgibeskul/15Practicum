1c696f84c1cc8a56f2d4ace38dfb426c17b3e338
vladgibeskul <wgv96300@gmail.com>
2024-12-25 09:13:47 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{/// <summary>
+/// Генерируются 10 случайных целых чисел в интервале (-40, 40). Вывести эти числа.
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        public static void Func()
+        {
+            Random random = new Random();
+            for (int i = 0; i<10; i++)
+            {
+                int number=random.Next(-40,40);
+                Console.WriteLine(number);
+            }
+        }
+
+    }
+}
