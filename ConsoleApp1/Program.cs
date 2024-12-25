98137f5df2a198f0a88c633fc73583be7b4dc9e8
vladgibeskul <wgv96300@gmail.com>
2024-12-25 08:47:02 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{/// <summary>
+/// Напечатать ряд чисел 20 в виде: 20 20 20 20 20 20 20 20 20 20.
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        public static void Func()
+        {
+            for (int x = 1; x <= 10; x++)
+            {
+                Console.Write($"{20}\t");
+            }
+        }
+    }
+}
