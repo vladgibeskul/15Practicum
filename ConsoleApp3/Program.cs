1ae2a967745a336ce7e626274d26bfa1f29a503b
vladgibeskul <wgv96300@gmail.com>
2024-12-25 09:05:31 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{/// <summary>
+///  Вывести четные двузначные числа и подсчитать их количество.
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        public static void Func()
+        {
+            int count = 0;
+            
+            for (int x = 10; x <= 99; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine(x);
+                    count++;
+                }
+                
+            }
+            Console.WriteLine($"count={count}");
+        }
+    }
+}
